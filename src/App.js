80d079fae46aa9075fcdfad95becd950e33d7653
CleverlyCodes <cleverly.codes@gmail.com>
2022-02-21 19:58:55 +0800

    add typing effect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import './App.css';
+import Typewriter from "typewriter-effect";
 
 function App() {
+  const things = [
+    'Websites', 
+    'Applications',
+    'Content',
+    'Security',
+    'Scripts',
+    'Artificial Intelligence',
+    'Blockchain'
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Cleverly Coding soon...</h1>
+        <h1>Cleverly Codes...</h1>
+
+        <Typewriter
+          options={{
+            strings: ['Websites', 'Applications', 'Content', 'Security', 'Scripts', 'Artificial Intelligence', 'Blockchain'],
+            autoStart: true,
+            pauseFor: 1000,
+            loop: true,
+          }}
+        />
       </header>
     </div>
   );
